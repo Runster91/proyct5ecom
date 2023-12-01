038c70c9c7ffb31e391e71cfcc3038f1690c1137
Runster91 <Guillermo.Casillas.l@outlook.com>
2023-11-30 18:13:11 -0600

    df3

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -16,6 +16,9 @@ export const readAll = async (req,res) => {
     }
 }
   
+
+
+
 export const create =async(req,res) => {
     try{
         const{name, email, phone, password} = req.body
@@ -67,8 +70,87 @@ export const create =async(req,res) => {
  }  
 }
 
+const login = async(req, res) => {
+
+    const {name, password } = req.body
+
+    try{
+        const foundUser = await User.findOne({ email })
+        console.log("foundUser", foundUser)
+
+        if(!foundUser){
+            return res.status(400).json({
+                msg: "El usuario o la contraseña no coinciden. Codigo: 5841"
+            })
+        }
+
+            const dbUserPassword = foundUser.password
+
+            const verifiedPass = await bcryptjs.compare(password, dbUserPassword)
+
+            if(!verified) {
+                return await res.status.json({
+                    msg: "El usuario o la contraseña no coinciden.Código  5845",
+                })
+            const payload = {
+                user: {
+                    id: foundUser._id,
+                },
+            }  
+            
+            jwt.sign(
+                payload,
+                process.env.JWT_SECRET,
+                {
+                    expiresin:360000
+                },
+                (err, token) => {
+                    if (error) {
+                        console.log("error", error)
+                        return new Error(error)
+                    }
+
+                    return res.json({
+                        msg: "Usuario con inicio de sesión exitoso",
+                        data: token,
+                    })
+                }
+            )
+            
+            }
+
+
+    } catch (error){
+        console.log("error", error)
+        res.status(500).json({
+            msg: "Hubo un problema de conexión",
+    })        
+}
+}
+
+const verifyToken = async(req, res) => {
+    try {
+        console.log("req.user", req.user)
+    const  foundUser = await User.findById(req.user.id)
+    console.log ("foundUser", foundUser)
+
+
+    return res.json({
+        msg: "Datos de usuario encontrados",
+        data: foundUser
+    })
+  } catch (error) {
+     console.log(error)
+     res.status(500).json({
+        msg: "el usuario no se  encontro "
+     })
+    }
+}
+
 export default {
 readAll,
 create,
+login,
+verifyToken,
 }
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 // 1. IMPORTACIONES
 // A. LIBRERÍAS
 import express from "express"
+import jwt from "jsonwebtoken"
 // B. ARCHIVOS
 import usersControllers from "./../controllers/usersControllers.js"
 
@@ -20,6 +21,46 @@ const router = express.Router()
 router.get("/", usersControllers.readAll)
 
 router.post("/create", usersControllers.create)
+
+router.post("/login", usersControllers.login)
+
+router.get(
+    "/verifytoken", 
+    async(req, res, next) => {
+
+    const token = req.header("x-auth-token")
+    console.log("token" , token)
+
+    if(!token){
+        return res.status(401).json({
+            msg: "No hay token o es  invalido, intente de en otro momento"
+        })
+    }
+
+    try{
+
+        console.log("token", token)
+        console.log("secret", process.env.JWT_SECRET)
+
+
+        const openToken = await jwt.verify(token, process.env.JWT_SECRET)
+        console.log("openToken", openToken),
+
+
+        req.user = openToken.user,
+
+        next()
+    } catch (error) {
+        res.status(500).json({
+            msg: "Hubo un error en servidor o token",
+        })
+    }
+
+}, 
+usersControllers.verifyToken
+) 
+
+
 
 // 4. EXPORTACIÓN
 export default router
