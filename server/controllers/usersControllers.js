e76b313b1797340d8ac5323c5abf21b1a250b91e
Runster91 <Guillermo.Casillas.l@outlook.com>
2023-12-07 22:54:43 -0600

    jdlikr1

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -1,156 +1,160 @@
-import User from "../models/User.js"
+import User from "./../models/User.js"
 import bcryptjs from "bcryptjs"
 import jwt from "jsonwebtoken"
+import Cart from "./../models/Cart.js"
 
-export const readAll = async (req,res) => {
-    try {
-
-        const users = await User.find()
-
-        res.json({
-            msg: "bikes displayed",
-            data: users,
-        })
-    } catch(error) {
-        console.log("error", error)
-    }
+const readAll = (req, res) => {
+  res.json({
+    message: "Datos obtenidos con éxito.",
+    data: data,
+  })
 }
-  
 
+const create = async (req, res) => {
+  const { name, lastname, country, address, zipcode, email, password } =
+    req.body
 
+  try {
+    // 1. CAPA DE CONTRASEÑA
+    // ESTABLECER EL NIVEL DE DIFICULTAD DE ENCRIPTAMIENTO DEL PASSWORD
+    const salt = await bcryptjs.genSalt(10)
 
-export const create =async(req,res) => {
-    try{
-        const{name, email, phone, password} = req.body
-        
-        const salt = await bcryptjs.genSalt(10)
+    // 1A. CREAR CARRITO DE COMPRAS
+    const newCart = await Cart.create({})
 
-        const hashedPassword = await bcryptjs.hash(password, salt)
+    // ENCRIPTAR EL PASSWORD
+    const hashedPassword = await bcryptjs.hash(password, salt)
+    console.log("hashedPassword", hashedPassword)
 
-        const newUser = await User.create({
-        name,
-        email,
-        phone,
-        password : hashedPassword, 
-        })
-
-        const payload = {
-            user: {
-                id: newUser_id,
-            }
-        }
-
-        jwt.sign (
-            payload,
-            process.env.JWT_SECRET,
-            {
-                expiresin: 360000
-            }, (error, token) => {
-                if (error){
-                    console.log("error", error)
-                    return new Error(error)
-                }
-                return res.json({
-                    msg: "user  added in cure manner",
-                    data: token,
-                })
-        
-
-
-            }
-        )
-
-        
-
-   } catch (error) {
-    console.log ("error", error)
-    res.status(500).json({
-        msg:"Hubo un error obteniendo los datos"
+    const newUser = await User.create({
+      name,
+      lastname,
+      country,
+      address,
+      zipcode,
+      email,
+      password: hashedPassword,
+      cart: newCart,
     })
- }  
-}
 
-const login = async(req, res) => {
+    // CAPA DE SEGURIDAD JWT
+    // 1. GENERAR UNA ELECCIÓN DE DATOS (ID) - PAYLOAD
+    const payload = {
+      user: {
+        id: newUser._id,
+      },
+    }
 
-    const {name, password } = req.body
-
-    try{
-        const foundUser = await User.findOne({ email })
-        console.log("foundUser", foundUser)
-
-        if(!foundUser){
-            return res.status(400).json({
-                msg: "El usuario o la contraseña no coinciden. Codigo: 5841"
-            })
+    // 2. ESTABLECER LA FIRMA JWT (SUPER SECRETA. SOLO LA TIENE EL CLIENTE Y EL SERVER)
+    return jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      {
+        expiresIn: 3600000,
+      },
+      (error, token) => {
+        if (error) {
+          console.log("error", error)
+          return new Error(error)
         }
 
-            const dbUserPassword = foundUser.password
-
-            const verifiedPass = await bcryptjs.compare(password, dbUserPassword)
-
-            if(!verified) {
-                return await res.status.json({
-                    msg: "El usuario o la contraseña no coinciden.Código  5845",
-                })
-            const payload = {
-                user: {
-                    id: foundUser._id,
-                },
-            }  
-            
-            jwt.sign(
-                payload,
-                process.env.JWT_SECRET,
-                {
-                    expiresin:360000
-                },
-                (err, token) => {
-                    if (error) {
-                        console.log("error", error)
-                        return new Error(error)
-                    }
-
-                    return res.json({
-                        msg: "Usuario con inicio de sesión exitoso",
-                        data: token,
-                    })
-                }
-            )
-            
-            }
-
-
-    } catch (error){
-        console.log("error", error)
-        res.status(500).json({
-            msg: "Hubo un problema de conexión",
-    })        
-}
+        return res.json({
+          msg: "Usuario creado con éxito y super seguro. guiño guiño",
+          data: token,
+        })
+      }
+    )
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      msg: "Hubo un problema en el servidor",
+      error,
+    })
+  }
 }
 
-const verifyToken = async(req, res) => {
-    try {
-        console.log("req.user", req.user)
-    const  foundUser = await User.findById(req.user.id)
-    console.log ("foundUser", foundUser)
+const login = async (req, res) => {
+  const { email, password } = req.body
 
+  try {
+    // 1. CONFIRMAMOS QUE EL USUARIO EXISTA EN BASE DE DATOS
+    const foundUser = await User.findOne({ email })
+    console.log("foundUser", foundUser)
+
+    if (!foundUser) {
+      return res.status(400).json({
+        msg: "El usuario o la contraseña no coinciden. Código: 5841",
+      })
+    }
+
+    // 2. EVALUACIÓN DEL PASSWORD
+    const dbUserPassword = foundUser.password
+
+    // password: holamundo123
+    // passworddb: $2a$10$KX.spNG28hut8r8G7gLMb.el9xcZXc9E1c/4iUf07AYs3F5KRJcaK
+    const verifiedPass = await bcryptjs.compare(password, dbUserPassword)
+
+    // UNA VEZ HECHO LA COMPARACIÓN, verifiedPass DEVOLVERÁ TRUE O FALSE
+    if (!verifiedPass) {
+      return await res.status.json({
+        msg: "El usuario o la contraseña no coinciden. Código: 5845",
+      })
+    }
+
+    // GENERAMOS DE CAPA DE SEGURIDAD JWT
+    const payload = {
+      user: {
+        id: foundUser._id,
+      },
+    }
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1y",
+      },
+      (error, token) => {
+        if (error) {
+          console.log("error", error)
+          return new Error(error)
+        }
+
+        return res.json({
+          msg: "Usuario con inicio de sesión exitoso.",
+          data: token,
+        })
+      }
+    )
+  } catch (error) {
+    console.log("error", error)
+    res.status(500).json({
+      msg: "Hubo un problema de conexión",
+    })
+  }
+}
+
+const verifyToken = async (req, res) => {
+  try {
+    console.log("req.user", req.user)
+    const foundUser = await User.findById(req.user.id)
+    console.log("foundUser", foundUser)
 
     return res.json({
-        msg: "Datos de usuario encontrados",
-        data: foundUser
+      msg: "Datos de usuario encontrados.",
+      data: foundUser,
     })
   } catch (error) {
-     console.log(error)
-     res.status(500).json({
-        msg: "el usuario no se  encontro "
-     })
-    }
+    console.log(error)
+    res.status(500).json({
+      msg: "El usuario no se encontró.",
+    })
+  }
 }
 
 export default {
-readAll,
-create,
-login,
-verifyToken,
+  readAll,
+  create,
+  login,
+  verifyToken,
 }
-
