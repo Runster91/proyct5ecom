8f4c6b0ff57d0f5852d25f3e2946c9a47975bfb8
Runster91 <Guillermo.Casillas.l@outlook.com>
2023-12-07 22:08:33 -0600

    jdlikr

--- server/controllers/checkoutController.js
+++ server/controllers/checkoutController.js
@@ -1,114 +1,184 @@
-import Cart from "../models/Cart.js";
-import stripe from "stripe";
-import dotenv from "dotenv";
+import stripe from "stripe"
+import dotenv from "dotenv"
+import User from "./../models/User.js"
+import Cart from "./../models/Cart.js"
+
 
 dotenv.config()
 
-const stripekey = stripe(process.env.STRIPE_SECRET_KEY)
+const stripeKey = stripe(process.env.STRIPE_SECRET_KEY)
 
-const createCheckoutSession = async(req, res) =>{
-    const user = {
-        id: "123",
-        email: "example@runster91.com"
-    }
+const createCheckoutSession = async (req, res) => {
+  // 1. OBTENER EL USUARIO
+  const userID = req.user.id
+  console.log(userID)
 
-const line_items= [
-    {
-        price: "price_1OJ9VsJ1UyaBsovqMVgHWa9v",
-        quantity: 1
+  // 2. BUSCAR EN BASE DE DATOS
+  const foundUser = await User.findById(userID).lean()
+  console.log(foundUser)
+
+  // 3. CREACIÓN DEL CARRITO DE COMPRAS U OBTENCIÓN DEL USUARIO
+
+  // const foundCart = await Cart.findById(foundUser.cart).lean() => 6558e1ff55fec44c463bd837
+  // const foundCart = await Cart.findById(foundUser.cart).lean().populate() => { products: [{_id:6558e1ff55fec44c463bd837, quantity: 4, priceID: "price_123u1231" }]
+
+  const foundCart = await Cart.findById(foundUser.cart).lean().populate()
+  console.log(foundCart)
+
+  // 4. ACOMODAR LOS DATOS DEL CARRITO PARA STRIPE
+  const line_items = foundCart.products.map((productToBuy) => {
+    return {
+      price: productToBuy.priceID,
+      quantity: productToBuy.quantity,
     }
-]
-try {
-    const session = await stripekey.checkout.sessions.create({
-        line_items,
-        mode: "payments",
-        succes_url: "https://buy.stripe.com/test_6oE5lyct46U9eDm9AB",
-        cancel_url: "https://google.com",
-        customer_email: "example@runster91.com",
+  })
+
+  console.log(line_items)
+
+  // 5. CREACIÓN DE CHECKOUT EN STRIPE
+  try {
+    const session = await stripeKey.checkout.sessions.create({
+      line_items,
+      mode: "payment",
+      success_url: "https://google.com",
+      cancel_url: "https://yahoo.com",
+      customer_email: foundUser.email,
     })
-
     console.log("session", session)
 
     res.status(200).json({
-        msg:"Accede a esta liga para pagar",
-        session_url: session.url,
-        session
+      msg: "Accede a este link para la sesión de pago",
+      session_url: session.url,
+      session,
     })
-} catch (error) {
+  } catch (error) {
     console.log("error", error)
     res.status(400).json({
-        msg: "Hubo un problema",
-        error,
+      msg: "Hubo un problema",
+      error,
     })
+  }
 }
 
-}
+// CREAR ORDEN
+// SE VA A RECIBIR UNA PETICIÓN POR STRIPE (NO POR THUNDERCLIENT) EL CUAL VA A INCLUIR
+// TODOS LOS DATOS DE LA ORDEN QUE HIZO EL USUARIO (YA LO PAGÓ) Y NOSOTROS
+// VAMOS A GENERAR EN BASE DE DATOS SU RECIBO.
+const createOrder = async (req, res) => {
+  try {
+    // 1. OBTENER LA FIRMA DE STRIPE SECRETA WEBHOOKS
+    // (SIEMPRE ES ASÍ)
+    const sig = req.headers["stripe-signature"]
+    const endpointSecret = process.env.STRIPE_WH_SIGNING_SECRET
+    console.log(sig)
+    console.log(endpointSecret)
 
-const createOrder = async (req,res) =>{
-    // const sig = req.headers["stripe-signature"]
-    // const endpointSecret = process.env.STRIPE_WH_SIGNIN_SE
-    // console.log(req.body)
-    // console.log(sig)
-    // console.log(endpointSecret)
-    
-    // let event
-    // try {
-    //     event =stripekey.webhooks.constructEvent(req.body, sig, endpointSecret)
-    // } catch (error) {
-    //     console.log("error", error)
-    //     res.status(400).json({
-    //         msg: error
-    //     })
-    // }
-    const paymentIntent = req.body.session.data.object
-    const email = paymentIntent.recipt_url
-    const receiptID = receiptURL.split("/").filter((item) => item).pop
-    const amount = paymentIntent.amount
-    const date_created = paymentIntent.created
+    // 2. CONSTRUIR EL EVENTO CON TODOS LOS DATOS SENSIBLES DE STRIPE
+    // EL EVENTO ES EL OBJETO QUE INCLUYE LOS RECIBOS Y LAS CONFIRMACIONES DE PAGO DEL USUARIO (DE SU ÚLTIMO STRIPE CHECKOUT)
+    let event = stripeKey.webhooks.constructEvent(req.body, sig, endpointSecret)
+    console.log(event)
 
-    const paymentDB = User.findOneAndUpdate({email}, 
-        {
+    // 3. EVALUAMOS EL EVENTO DE STRIPE
+    switch (event.type) {
+      // A. SI EL EVENTO FUE UN CARGO EXITOSO AL USUARIO
+      case "charge.succeeded":
+        // GENERAR VARIABLES PARA ARMAR NUESTRO GUARDADO EN BASE DE DATOS
+        const paymentIntent = event.data.object
+        console.log(paymentIntent)
+
+        const email = paymentIntent.billing_details.email
+        console.log(email)
+
+        const receiptURL = paymentIntent.receipt_url // https://receipt.stripe.com/12312/!2312/23123
+        console.log(receiptURL)
+
+        const receiptID = receiptURL
+          .split("/")
+          .filter((item) => item)
+          .pop() // !2312
+        console.log(receiptID)
+
+        const amount = paymentIntent.amount
+        console.log(amount)
+
+        const date_created = paymentIntent.created
+        console.log(date_created)
+
+        const paymentDB = await User.findOneAndUpdate(
+          { email },
+          {
             $push: {
-                receipts:{
-                    receiptURL,
-                    receiptID,
-                    date_created,
-                    amount,
-                },
+              receipts: {
+                receiptURL,
+                receiptID,
+                date_created,
+                amount,
+              },
             },
-        }, 
+          },
+          { new: true }
+        ).lean()
 
-        {new: true}
-        )
+        console.log(paymentDB)
+        res.status(200).json({
+          msg: "Datos actualizados con éxito. Pago correcto.",
+        })
 
-    console.log ("req.body", req.body)
-    console.log("req.body", req.body.session)
-    try {
-        switch(true){
-            case "charge.succeeded":
-                default: 
-                console.log("Evento no encontrado")
+        break
 
-                res.status(200).json({
-                    msg: "evento no encontrado",
-                })
-
-               
-
-        }
-    } catch (error) {
-        console.log ("error", error)
-        res.status(400).json({
-            msg:error,
+      default:
+        console.log("Evento sin coincidencia.")
+        res.status(200).json({
+          msg: "Evento sin coincidencia",
         })
     }
 
-    res.status(200).json({
-        msg: "Datos de Stripe recibidos.",
+    return
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      msg: "Hubo un problema en la generación de recibos para el usuario.",
     })
+  }
 }
 
-export default{
-    createCheckoutSession,
-    createOrder
+const editCart = async (req, res) => {
+  console.log(req.user)
+  console.log(req.body)
+  // OBTENER EL ID DEL USUARIO
+  const userID = req.user.id
+
+  try {
+    // ENCONTRAR EL USUARIO EN BASE DE DATOS
+    console.log(userID)
+    const foundUser = await User.findById(userID).lean()
+
+    // OBTENER EL LISTADO DE PRODUCTOS CON DATOS
+    const { products } = req.body
+
+    // ACTUALIZAR EL CARRITO
+    const updatedCart = await Cart.findByIdAndUpdate(
+      foundUser.cart,
+      { products },
+      { new: true }
+    )
+
+    res.status(200).json({
+      msg: "Carrito actualizado",
+      updatedCart,
+    })
+  } catch (error) {
+    console.log(error)
+    console.log(error)
+    return res.status(500).json({
+      msg: "Hubo un error en servidor",
+      error,
+    })
+  }
+}
+
+export default {
+  createOrder,
+  createCheckoutSession,
+  editCart,
 }
